f48c241efaabaae5f00934b08d1c17854b551a0d
NjeriMarion <njerimarion@gmail.comgit config --global user.email njerimarion@gmail.comhistorygit clone https://github.com/NjeriMarion/tools_new_2.git>
2023-05-16 23:05:35 +0300

    changes

--- available_products.py
+++ available_products.py
@@ -5,8 +5,9 @@ class AvailableGroceries:
     def __init__(self,item_name,price,addtobasket, available_groceries) :
         self.item_name=item_name
         self.price=price
-        self.addtobasket=addtobasket
-        self.groceries = available_groceries
+        self.addtobasket={}
+        self.groceries = {}
+        
     def add_to_cart(self,add):
         self.add=self.addtobasket
 
@@ -14,9 +15,11 @@ class AvailableGroceries:
 
         if self.item_name in self.groceries and self.price in self.groceries:
             self.add==True
+            print(f"{self.item_name} added to basket")
             
         else:
             self.add == False
+            print(f"{self.item_name} is not available")
 
 #**************** returning the final state of the button "add to cart"******************
 
